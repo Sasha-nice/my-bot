aceaca9d12705ee886ec718b3f0314c6d6e5d5af
Sasha-nice <alaborisov@Alexanders-MacBook-Pro.local>
2024-01-08 22:05:33 +0300

    other lib

--- main.py
+++ main.py
@@ -1,38 +1,13 @@
-import asyncio
-import logging
+import telebot
 
-from aiogram import Bot, Dispatcher
-from aiogram.enums.parse_mode import ParseMode
-from aiogram.fsm.storage.memory import MemoryStorage
-
-
-from aiogram import types, F, Router
-from aiogram.types import Message
-from aiogram.filters import Command
 from settings import Config
 
-
-router = Router()
-
-
-@router.message(Command("start"))
-async def start_handler(msg: Message):
-    await msg.answer("Привет! Я помогу тебе узнать твой ID, просто отправь мне любое сообщение")
+bot = telebot.TeleBot(Config.TOKEN)
 
 
-@router.message()
-async def message_handler(msg: Message):
-    await msg.answer(f"Твой ID: {msg.from_user.id}")
+@bot.message_handler(commands=["start"])
+def start_message(message):
+    bot.send_message(message.chat.id, "Привет ✌️ ")
 
 
-async def main():
-    bot = Bot(token=Config.TOKEN, parse_mode=ParseMode.HTML)
-    dp = Dispatcher(storage=MemoryStorage())
-    dp.include_router(router)
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    asyncio.run(main())
+bot.infinity_polling()
